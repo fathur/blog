8ac78eae239ead9b568e24e19625e770baa49807
fathur <fathur_rohman17@yahoo.co.id>
2014-10-02 05:08:31 +0700

    Add Sentry to App provider and ALias

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'Cartalyst\Sentry\SentryServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 
 	),
@@ -190,6 +191,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+
+		'Sentry'		=> 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
